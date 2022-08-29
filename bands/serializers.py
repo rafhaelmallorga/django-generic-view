dc267dc96e5bb13efe3adf9438760228f1f24bca
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-08-29 11:31:34 -0300

    feat :sparkles: created album serializer and view

--- bands/serializers.py
+++ bands/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import Band
+from .models import Album, Band
 
 
 class BandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Band
+        fields = "__all__"
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Album
         fields = "__all__"
--- bands/views.py
+++ bands/views.py
@@ -1,11 +1,22 @@
 from rest_framework.views import APIView, Request, Response, status
 
-from bands.serializers import BandSerializer
+from bands.serializers import AlbumSerializer, BandSerializer
 
 
 class BandView(APIView):
     def post(self, request: Request) -> Response:
         serializer = BandSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class AlbumView(APIView):
+    def post(self, request: Request) -> Response:
+        serializer = AlbumSerializer(data=request.data)
 
         serializer.is_valid(raise_exception=True)
 
